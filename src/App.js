d0698e243942756362c4870b6b8ec9f400be66cd
freetimezp <kraftukrnet@ukr.net>
2023-04-07 21:45:24 +0300

    05. js-sport-03. Plans component.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Hero from './components/Hero/Hero';
 import Programs from './components/Programs/Programs';
 import Reasons from './components/Reasons/Reasons';
+import Plans from './components/Plans/Plans';
 
 import './App.css';
 
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <Programs />
       <Reasons />
+      <Plans />
     </div>
   );
 }
---
+++ src/components/Plans/Plans.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import { plansData } from '../../data/plansData';
+import whiteTick from '../../assets/whiteTick.png';
+
+import './Plans.css';
+
+const Plans = () => {
+    return (
+        <div className='plans-container'>
+            <div className="programs-header" style={{ gap: '2rem' }}>
+                <span className='stroke-text'>Ready to start</span>
+                <span>your journy</span>
+                <span className='stroke-text'>now with us</span>
+            </div>
+
+            <div className="plans">
+                {plansData.map((plan, index) => (
+                    <div className='plan' key={index}>
+                        {plan.icon}
+                        <span>{plan.name}</span>
+                        <span>$ {plan.price}</span>
+
+                        <div className="features">
+                            {plan.features.map((feature, i) => (
+                                <div className='feature' key={i}>
+                                    <img src={whiteTick} alt="white tick" />
+                                    <span key={i}>{feature}</span>
+                                </div>
+                            ))}
+                        </div>
+
+                        <div>
+                            <span>See more benefits &#8594; </span>
+                        </div>
+                        <button className='btn'>Join Now</button>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Plans;
