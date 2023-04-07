6dcc162bc8987ed5fd4dade7f2174b32e085321f
freetimezp <kraftukrnet@ukr.net>
2023-04-07 18:24:14 +0300

    03. js-sport-03. Programs component.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Hero from './components/Hero/Hero';
+import Programs from './components/Programs/Programs';
 
 import './App.css';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Hero />
+      <Programs />
     </div>
   );
 }
